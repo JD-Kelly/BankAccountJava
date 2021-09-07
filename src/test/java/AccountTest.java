390486c2f4f850758fda594da315a63dfdffbef0
Jonny Kelly <jonny.kelly192@gmail.com>
2021-09-07 14:43:03 +0100

    minor refactoring

--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -1,20 +1,20 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 public class AccountTest {
+    Account account = new Account();
 
     @Test
     public void testBalance () {
-        Account account = new Account();
         Integer result = account.getBalance();
         assertEquals(0, result);
     }
 
     @Test
     public void testDeposit () {
-        Account account = new Account();
         account.deposit(100);
         Integer result = account.getBalance();
         assertEquals(100, result);
@@ -22,7 +22,6 @@ public class AccountTest {
 
     @Test
     public void testWithdraw() {
-        Account account = new Account();
         account.deposit(100);
         account.withdraw(50);
         Integer result = account.getBalance();
@@ -31,7 +30,6 @@ public class AccountTest {
 
     @Test
     void testExceptionIsThrownIfWithdrawalTakesBalanceBelowZero() {
-        Account account = new Account();
         account.deposit(100);
         assertThrows(ArithmeticException.class, () -> account.withdraw(150));
     }
