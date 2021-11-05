7504bb8e82ad95bcaebefe53525ac9fa9e833c33
Jonny Kelly <jonny.kelly192@gmail.com>
2021-11-05 12:26:49 +0000

    PrinterTest now passes

--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -10,6 +10,6 @@ public class PrinterTest {
         statement.recordTransaction(100, LocalDate.of(2021, 1, 12));
         statement.recordTransaction(-50, LocalDate.of(2021, 1, 12));
         assertEquals("date || credit || debit || balance\n" + "12/01/2021 || 100.00 || - || 100.00\n"
-                + "12/01/2021 || - || 50.00 || 50.00\n", Printer.print(statement));
+                + "12/01/2021 || - || 50.00 || 50.00", Printer.print(statement));
     }
 }
