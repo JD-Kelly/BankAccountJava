47d1ca76878dd8dc5d1a4f8f02a399b621f0a382
Jonny Kelly <jonny.kelly192@gmail.com>
2021-11-05 12:03:32 +0000

    refactoring of test printer

--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -7,12 +7,9 @@ public class PrinterTest {
 
     @Test
     void testPrintsStatement() {
-        statement.recordTransaction(100, LocalDate.of(2021, 1,12));
-        statement.recordTransaction(-50, LocalDate.of(2021, 1,12));
-        assertEquals("""
-                date || credit || debit || balance
-                12/01/2021 || 100.00 || - || 100.00
-                12/01/2021 || - || 50.00 || 50.00""",
-                Printer.print(statement));
+        statement.recordTransaction(100, LocalDate.of(2021, 1, 12));
+        statement.recordTransaction(-50, LocalDate.of(2021, 1, 12));
+        assertEquals("date || credit || debit || balance\n" + "12/01/2021 || 100.00 || - || 100.00\n"
+                + "12/01/2021 || - || 50.00 || 50.00\n", Printer.print(statement));
     }
 }
